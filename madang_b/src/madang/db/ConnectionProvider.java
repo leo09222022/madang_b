3a38d9d3bcd10d6f28240cf0c565362dc6ff6a56
Sana2d2v <sana2d2v@gmail.com>
2025-04-02 11:53:47 +0900

    feat: 커넥션프로바이더 완료

--- madang_b/src/madang/db/ConnectionProvider.java
+++ madang_b/src/madang/db/ConnectionProvider.java
@@ -1,5 +1,65 @@
 package madang.db;
 
-public class ConnectionProvider {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class ConnectionProvider {
+	//공통적인 부분을 멤버변수로 만듬
+	public static String driver ="oracle.jdbc.driver.OracleDriver";
+	public static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	public static String user = "c##madang";
+	public static String pwd = "madang";
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			 Class.forName(driver);
+		     conn = DriverManager.getConnection(url, user, pwd);
+		     
+		} catch (Exception e) {
+			System.out.println("예외발생 :" + e.getMessage());
+		}
+		
+		return conn;
+	}
+	public static void close(Connection conn, Statement stmt , ResultSet rs  ) {
+		
+		try {
+			if( rs !=null ) {
+				rs.close();
+			}
+			if( stmt != null ) {
+				stmt.close();
+			}
+			if( conn != null ) {
+				conn.close();
+			}
+			
+		} catch (Exception e) {
+			System.out.println("예외발생 :" + e.getMessage());
+		}
+		
+		
+	}
+	
+	public static void close(Connection conn, Statement stmt  ) {
+		
+	try {
+		if( stmt != null ) {
+			stmt.close ();
+		}
+		if( conn != null ) {
+			conn.close();
+		}
+			
+		} catch (Exception e) {
+			System.out.println("예외발생 :" + e.getMessage());
+		}
+		
+	}
 }
